9fd6eef2c3ad9d4f4394fcfc23672cba7434e4d1
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-02-28 03:05:52 -0600

    inital commit

---
+++ Gesture_Recognition/gesture.py
@@ -0,0 +1,302 @@
+from __future__ import print_function, division, absolute_import
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import sys
+
+sys.path.append('../nvGesture')
+import readdata
+
+
+# Importing pretrained C3D Model from pickle
+# Setting up the network to work with my video data
+# Currently, (240, 320, 3, 80) x 1050 training
+# This network takes an input of 3x16x112x112
+# Todo: put this in multiple files
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+classes = 25
+batch_size = 10
+
+class C3D(nn.Module):
+    """
+    The C3D network as described in [1].
+    """
+
+    def __init__(self):
+        super(C3D, self).__init__()
+
+        # add more layers in the future to adapt to the right dimensions
+        # conv_input = tf.reshape(data,[-1,8,112,112,3]);
+
+        # self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))  # same
+        self.conv1 = nn.Conv3d(8, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))  # same
+        self.norm1a = nn.LayerNorm([64, 112, 112, 3])
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)) # same
+
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm2a = nn.LayerNorm([128, 112, 56, 1])
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 1), stride=(2, 2, 1)) # same
+
+        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm3a = nn.LayerNorm([256, 56, 28, 1])
+        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm3b = nn.LayerNorm([256, 56, 28, 1])
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 1), stride=(2, 2, 1)) # same (2,2,2)
+
+        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm4a = nn.LayerNorm([512, 28, 14, 1])
+        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm4b = nn.LayerNorm([512, 28, 14, 1])
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 1), stride=(2, 2, 1)) # same
+
+        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm5a = nn.LayerNorm([512, 14, 7, 1])
+        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)) # same
+        self.norm5b = nn.LayerNorm([512, 14, 7, 1])
+        # self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(1, 1, 1), padding=(0, 1, 1)) # This was removed
+        self.fc6_size = 512 * 14 * 7 * 1
+        self.fc6 = nn.Linear(self.fc6_size, 512)  # 50176
+        self.fc7 = nn.Linear(512, 512)
+        # self.fc8 = nn.Linear(4096, 487)
+
+        self.dropout = nn.Dropout(p=0.5)
+
+        self.relu = nn.ReLU()
+        # self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+
+        print("initial shape!")
+        print(x.shape)
+        test = x[:,1,:,:,:]
+        print(test.shape)
+        h = self.relu(self.conv1(x))
+        # print("AFTER CONV1")
+        # print(h.shape)
+        h = self.relu(self.norm1a(h))
+        h = self.pool1(h)
+
+        h = self.relu(self.conv2(h))
+        # print("AFTER CONV2")
+        # print(h.shape)
+        h = self.relu(self.norm2a(h))
+        h = self.pool2(h)
+
+        h = self.relu(self.conv3a(h))
+        h = self.relu(self.norm3a(h))
+        h = self.relu(self.conv3b(h))
+        h = self.relu(self.norm3b(h))
+        h = self.pool3(h)
+
+        h = self.relu(self.conv4a(h))
+        h = self.relu(self.norm4a(h))
+        h = self.relu(self.conv4b(h))
+        h = self.relu(self.norm4b(h))
+        h = self.pool4(h)
+
+        h = self.relu(self.conv5a(h))
+        # print("conv5a output shape")
+        # print(h.shape)
+        # h = h.view(-1, 512, 7, 7, 1)
+        h = self.relu(self.norm5a(h))
+        h = self.relu(self.conv5b(h))
+        # print("conv5b output shape")
+        # print(h.shape)
+        h = self.relu(self.norm5b(h))
+        # h = self.pool5(h) # This was removed
+        # print("norm5b output shape")
+        # print(h.shape)
+        # print("first forward")
+
+        h = h.view(-1, self.fc6_size) # 50176
+        h = self.relu(self.fc6(h))
+        h = self.dropout(h)
+        h = self.relu(self.fc7(h))
+        h = self.dropout(h)
+
+        # logits = self.fc8(h)
+        # probs = self.softmax(h)
+        probs = h
+
+        return probs
+
+    # Adapting C3D to my data
+
+class classifier(nn.Module):
+
+    def __init__(self):
+        super(classifier, self).__init__()
+        
+        self.ltsm = nn.LSTMCell(512, 512)
+
+        self.fc1 = nn.Linear(512,25) # should be 512*2?
+        # self.lin1 = nn.Linear(487, 256)
+        # self.bn = nn.BatchNorm3d(256)
+        # self.dropout = nn.Dropout(0.15)
+        # self.lin2 = nn.Linear(256,125)
+        # self.lin3 = nn.Linear(125,25)
+        # self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+    
+
+    # Not sure exactly how this is supposed to work. Got this from a tutorial
+    def forward(self, x):
+        
+        print("second initial shape!")
+        print(x.shape)
+        # test = x[:,1,:,:,:]
+        # print(test.shape)
+
+        h = x.view(-1,512)
+        h, _ = self.ltsm(h)
+        # print("LTSM shape")
+        # print(h.shape)
+        # h = h.view(-1, 512)
+        logits = self.fc1(h)
+        probs = self.softmax(logits)
+        # logits = logits.view(-1,4,25)
+
+        return probs
+
+net = C3D()
+# net.load_state_dict(torch.load('c3d.pickle')) # experiment with commenting this out and not
+net = nn.Sequential(
+        net, classifier()
+)
+
+net = nn.DataParallel(net)
+net.to(device)
+print(net.device_ids)
+
+
+# from torchsummary import summary
+# summary(net, (8,112,112,3), batch_size=batch_size)
+# summary(net, (3,8,112,112), batch_size=batch_size)
+
+train_loader, test_loader = readdata.MakeDataloaders()
+
+
+def saveModel(): 
+    path = "./network.pth" 
+    torch.save(net.state_dict(), path) 
+
+"""
+References
+----------
+[1] Tran, Du, et al. "Learning spatiotemporal features with 3d convolutional networks." 
+Proceedings of the IEEE international conference on computer vision. 2015.
+"""
+
+
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.CTCLoss(reduction='mean', zero_infinity=True) # Try "none" here
+optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum=0.9)
+# optimizer = optim.Adam(net.parameters(), lr = 0.001)
+
+print("did it make it here?")
+# print(net.device)
+
+epochs = 100
+train_accu = []
+train_losses = []
+hasprint = True
+# input_lengths = torch.full(size=(batch_size,), fill_value=8, dtype=torch.long)
+# target_lengths = torch.full(size=(batch_size,), fill_value=26, dtype=torch.long)
+# log_probs = 8, 10, 26 (HAS BLANK)
+input_lengths = torch.tensor(batch_size);
+target_lengths = torch.tensor(batch_size);
+writer = SummaryWriter()
+
+for epoch in range(1, epochs+1):
+    print("\nEpoch : %d"%epoch)
+    running_train_loss = 0.0
+    running_val_loss = 0.0
+    running_accuracy = 0.0
+    total = 0
+    # Loop through all the data
+    for i, data in enumerate(train_loader,0):
+        # print("batch?")
+        # print(i)
+        # print(data)
+        inputs, labels = data
+        if hasprint:
+            print("inputs and labels shapes")
+            print(inputs.shape)
+            print(labels.shape)
+            hasprint = False
+        if torch.cuda.is_available():
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        
+
+        # print(device)
+        # print(inputs.device)
+        # print(labels.device)
+
+
+        optimizer.zero_grad()
+        
+        outputs = net(inputs)
+        print("SHAPES")
+        print(outputs.shape)
+        # print(outputs)
+        print(labels.shape)
+        print(labels)
+        
+        
+        # log_probs = 8, 10, 26 (HAS BLANK)
+        # labels = [10, 8] tensor, each frame has the label eg: [5, 5, 5, 5, 5, 5, 5, 5]
+        # input_lengths = torch.tensor(batch_size); - batch size length with all 8s
+        # target_lengths = torch.tensor(batch_size); - batch size length with all 8s
+        # train_loss = criterion(outputs, labels)
+        train_loss = criterion(outputs, labels, input_lengths, target_lengths)
+        train_loss.backward()
+        optimizer.step()
+
+        running_train_loss += train_loss.item()
+
+    train_loss_value = running_train_loss/len(train_loader)
+
+    with torch.no_grad(): 
+        net.eval() 
+        for data in test_loader:
+            inputs, outputs = data 
+            if torch.cuda.is_available():
+                inputs = inputs.cuda()
+                outputs = outputs.cuda()
+            predicted_outputs = net(inputs)
+            val_loss = criterion(predicted_outputs, outputs)
+            
+            # The label with the highest value will be our prediction 
+            _, predicted = torch.max(predicted_outputs, 1) 
+            running_val_loss += val_loss.item()
+            total += outputs.size(0) 
+            running_accuracy += (predicted == outputs).sum().item() 
+
+        # Calculate validation loss value 
+        val_loss_value = running_val_loss/len(test_loader)
+                
+        # Calculate accuracy as the number of correct predictions in the validation batch divided by the total number of predictions done.  
+        accuracy = (100 * running_accuracy / total)     
+ 
+        # Save the model if the accuracy is the best 
+        # if accuracy > best_accuracy: 
+            # saveModel() 
+            # best_accuracy = accuracy 
+         
+        # Print the statistics of the epoch 
+        print('Completed training batch', epoch, 'Training Loss is: %.4f' %train_loss_value, 'Validation Loss is: %.4f' %val_loss_value, 'Accuracy is %.4f %%' % (accuracy))
+
+    # # https://androidkt.com/calculate-total-loss-and-accuracy-at-every-epoch-and-plot-using-matplotlib-in-pytorch/
+    # # https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-analysis-train-model
---
+++ nvGesture/readdata.py
@@ -0,0 +1,268 @@
+import cv2
+import numpy as np
+import pdb
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+def load_split_nvgesture(file_with_split ,list_split = list()):
+    params_dictionary = dict()
+    with open(file_with_split,'rt') as f:
+          dict_name  = file_with_split[file_with_split.rfind('/')+1 :]
+          dict_name  = dict_name[:dict_name.find('_')]
+
+          for line in f:
+            params = line.split(' ')
+            params_dictionary = dict()
+
+            params_dictionary['dataset'] = dict_name
+
+            path = params[0].split(':')[1]
+            for param in params[1:]:
+                    parsed = param.split(':')
+                    key = parsed[0]
+                    # print(key)
+                    if key == 'label':
+                        # make label start from 0
+                        label = int(parsed[1]) - 1 
+                        params_dictionary['label'] = label
+                    elif key in ('depth','color','duo_left'):
+                        #othrwise only sensors format: <sensor name>:<folder>:<start frame>:<end frame>
+                        sensor_name = key
+                        #first store path
+                        params_dictionary[key] = path + '/' + parsed[1]
+                        #store start frame
+                        params_dictionary[key+'_start'] = int(parsed[2])
+
+                        params_dictionary[key+'_end'] = int(parsed[3])
+                        # print(params_dictionary)
+        
+            # print(params_dictionary)
+            params_dictionary['duo_right'] = params_dictionary['duo_left'].replace('duo_left', 'duo_right')
+            params_dictionary['duo_right_start'] = params_dictionary['duo_left_start']
+            params_dictionary['duo_right_end'] = params_dictionary['duo_left_end']          
+
+            params_dictionary['duo_disparity'] = params_dictionary['duo_left'].replace('duo_left', 'duo_disparity')
+            params_dictionary['duo_disparity_start'] = params_dictionary['duo_left_start']
+            params_dictionary['duo_disparity_end'] = params_dictionary['duo_left_end']                  
+
+            list_split.append(params_dictionary)
+ 
+    return list_split
+
+def load_data_from_file(example_config, sensor,image_width, image_height):
+
+    path = "../nvGesture/" + example_config[sensor] + ".avi"
+    start_frame = example_config[sensor+'_start']
+    # print(start_frame)
+    end_frame = example_config[sensor+'_end']
+    # print(end_frame)
+    label = example_config['label']
+
+    frames_to_load = range(start_frame, end_frame)
+
+    chnum = 3 if sensor == "color" else 1
+
+    video_container = np.zeros((image_height, image_width, chnum, 80), dtype = np.uint8)
+
+    cap = cv2.VideoCapture(path)
+
+    ret = 1
+    frNum = 0
+    cap.set(1, start_frame);
+    for indx, frameIndx in enumerate(frames_to_load):    
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.resize(frame,(image_width, image_height))
+            if sensor != "color":
+                frame = frame[...,0]
+                frame = frame[...,np.newaxis]
+            video_container[..., indx] = frame
+        else:
+            print("Could not load frame")
+            
+    cap.release()
+    # print("frames", len(frames_to_load))
+
+    return video_container, label
+
+
+class NVGesture():
+        def __init__(self, data, target, transform=None):
+            self.data = [torch.from_numpy(chunk).float() for chunk in data] # This will be the 1050*5 x 3 x 16 x 112 x 112 data?
+            self.target = target#torch.FloatTensor(target).long #torch.from_numpy(target).long
+            # print(self.data)
+            # print(self.target)
+            self.transform = transform
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self,index):
+            x = self.data[index]
+            y = self.target[index]
+
+            return x, y
+
+
+def MakeDataloaders():
+    
+    sensors = ["color", "depth", "duo_left", "duo_right", "duo_disparity"]
+    file_lists = dict()
+    file_lists["test"] = "../nvGesture/nvgesture_test_correct_cvpr2016.lst"
+    file_lists["train"] = "../nvGesture/nvgesture_train_correct_cvpr2016.lst"
+    # file_lists["test"] = "./nvgesture_test_correct_cvpr2016.lst"
+    # file_lists["train"] = "./nvgesture_train_correct_cvpr2016.lst"
+    train_list = list()
+    test_list = list()
+
+    load_split_nvgesture(file_with_split = file_lists["train"],list_split = train_list)
+    # print(train_list[0])
+    print(len(train_list))
+    load_split_nvgesture(file_with_split = file_lists["test"],list_split = test_list)
+    # print(len(test_list))
+
+    train_data = []
+    test_data = []
+    train_targets = []
+    test_targets = []
+
+    for train in range(10): # len(train_list)
+        data, label = load_data_from_file(example_config = train_list[train], sensor = sensors[0], image_width = 320, image_height = 240)
+        # print(data.shape)
+        # print(label)
+        print(train)
+        #print(train_list[train])
+        data = data[64:176, 104:216, :, :]
+        data2 = np.moveaxis(data,-1,0)
+        # data3 = np.moveaxis(data2,-1,0)
+
+        # print(data.shape)
+        print(data2.shape)
+
+        data_split = np.vsplit(data2,10) # split into 10 groups
+
+        # print(len(data_split))
+        # print(data_split[0].shape)
+
+        for chunk in range(len(data_split)):
+            train_data.append(data_split[chunk])
+            train_targets.append(label)
+
+
+    for test in range(10): # len(test_list)
+        data, label = load_data_from_file(example_config = test_list[test], sensor = sensors[0], image_width = 320, image_height = 240)
+        # print(data.shape)
+        # print(label)
+        print(test)
+        data = data[64:176, 104:216, :, :]
+        data2 = np.moveaxis(data,-1,0)
+        # data3 = np.moveaxis(data2,-1,0)
+
+        # print(data.shape)
+        print(data2.shape)
+
+        data_split = np.vsplit(data2,10) # split into 10 groups
+
+        # print(len(data_split))
+        # print(data_split[0].shape)
+
+        for chunk in range(len(data_split)):
+            test_data.append(data_split[chunk])
+            test_targets.append(label)
+
+    # print(len(train_data))
+    # print(train_data[0].shape)
+    # print(train_targets)
+
+
+    train_loader = torch.utils.data.DataLoader(dataset=NVGesture(train_data, train_targets), batch_size=10, shuffle=False, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(dataset=NVGesture(test_data, test_targets), batch_size=10, shuffle=False, pin_memory=True)
+    print("done loading data")
+
+    return train_loader, test_loader
+
+
+
+
+if __name__ == "__main__":
+
+    # sensors = ["color", "depth", "duo_left", "duo_right", "duo_disparity"]
+    # file_lists = dict()
+    # file_lists["test"] = "./nvgesture_test_correct_cvpr2016.lst"
+    # file_lists["train"] = "./nvgesture_train_correct_cvpr2016.lst"
+    # train_list = list()
+    # test_list = list()
+
+    # load_split_nvgesture(file_with_split = file_lists["train"],list_split = train_list)
+    # # print(train_list[0])
+    # print(len(train_list))
+    # load_split_nvgesture(file_with_split = file_lists["test"],list_split = test_list)
+    # # print(len(test_list))
+
+    # train_data = []
+    # test_data = []
+    # train_targets = []
+    # test_targets = []
+
+    # for train in range(1): # len(train_list)
+    #     data, label = load_data_from_file(example_config = train_list[train], sensor = sensors[0], image_width = 320, image_height = 240)
+    #     # print(data.shape)
+    #     # print(label)
+    #     data = data[64:176, 104:216, :, :]
+    #     data2 = np.moveaxis(data,-1,0)
+    #     data3 = np.moveaxis(data2,-1,0)
+
+    #     # print(data.shape)
+    #     # print(data3.shape)
+
+    #     data_split = np.hsplit(data3,5) # split into 5 groups
+
+    #     # print(len(data_split))
+    #     # print(data_split[0].shape)
+
+    #     for chunk in range(len(data_split)):
+    #         train_data.append(data_split[chunk])
+    #         train_targets.append(label)
+
+
+    # for test in range(1): # len(test_list)
+    #     data, label = load_data_from_file(example_config = train_list[test], sensor = sensors[0], image_width = 320, image_height = 240)
+    #     # print(data.shape)
+    #     # print(label)
+    #     data = data[64:176, 104:216, :, :]
+    #     data2 = np.moveaxis(data,-1,0)
+    #     data3 = np.moveaxis(data2,-1,0)
+
+    #     # print(data.shape)
+    #     # print(data3.shape)
+
+    #     data_split = np.hsplit(data3,5) # split into 5 groups
+
+    #     # print(len(data_split))
+    #     # print(data_split[0].shape)
+
+    #     for chunk in range(len(data_split)):
+    #         test_data.append(data_split[chunk])
+    #         test_targets.append(label)
+
+    # print(len(train_data))
+    # print(train_data[0].shape)
+    # print(train_targets)
+
+
+    # train_loader = torch.utils.data.DataLoader(dataset=NVGesture(train_data, train_targets), batch_size=1, shuffle=False)
+
+    train_loader, test_loader = MakeDataloaders()
+    print(train_loader)
+    # train_features, train_labels = next(iter(train_loader))
+    # print(train_labels)
+
+    
+    # This shows what the 80 - frame videos are doing!
+    # for i in range(80):
+        # frame = data[i,:,:,:]
+        # cv2.imshow("frame", frame)
+        # cv2.waitKey(50)
